9e10a94d6fdbcef3c050b830d44ec7563b1f8c00
busrabek <busrabekciogullari@gmail.com>
2021-01-17 20:39:08 +0100

    Pyhon if-statement Assignment added

---
+++ if-statement.py
@@ -0,0 +1,7 @@
+name = input("Please write your name: ").capitalize()
+name_in_system = "Busra"
+
+if name == name_in_system:
+    print("Hello Busra! The password is: C7178")
+else:
+    print("Hello {}! See you later.".format(name))
